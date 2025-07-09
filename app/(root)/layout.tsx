02217db7e2ee55920e44f7d6581800fc1bbf8455
Himanshu gaur <himanshugaur055@gmail.com>
2025-07-09 21:01:18 +0530

    Fresh clean start without secret

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+
+  return( <div className="root-layout">
+    <nav>
+      <Link href='/' className="flex items-center gap-2 ">
+      <Image src="/logo.svg" alt='logo' width={38} height={32}/>
+      <h2 className='text-primary-100'>MockMate</h2>
+
+      </Link>
+    </nav>
+    {children}
+  </div>
+)
+};
+
+export default RootLayout;
